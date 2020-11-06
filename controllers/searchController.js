e455e023ff666b66bfa6b0ac5031914458f684c3
acharjeeauntor <acharjeeauntor@gmail.com>
2020-11-06 17:16:34 +0600

    Complete Sorting and Seraching Section

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -20,10 +20,11 @@ exports.searchResult = (req, res) => {
     //var category = req.body.ctg;
     var pName = req.body.name;
     var name = pName.replace(/\s/g, "").toLowerCase()
+    var LName=pName.toLowerCase()
 
 //select * from testdb where Catagory LIKE "%${category}" AND product_name LIKE "%${pName}" ORDER BY price, rating DESC LIMIT 0,3
 
-con.query(`select * from testdb where product_name LIKE "%${name}" ORDER BY price, rating DESC LIMIT 0,3`,function(error,result){
+con.query(`select * from testdb where LOWER(product_name) LIKE "%${LName}" ORDER BY price, rating DESC LIMIT 0,3`,function(error,result){
    
     console.log(result.length)
     
@@ -35,3 +36,73 @@ con.query(`select * from testdb where product_name LIKE "%${name}" ORDER BY pric
     }
 })
 }
+
+
+exports.searchResultByRating = (req, res) => {
+    //var category = req.body.ctg;
+    var pName = req.body.name;
+    var name = pName.replace(/\s/g, "").toLowerCase()
+    var LName=pName.toLowerCase()
+
+//select * from testdb where Catagory LIKE "%${category}" AND product_name LIKE "%${pName}" ORDER BY price, rating DESC LIMIT 0,3
+
+con.query(`select * from testdb where LOWER(product_name) LIKE "%${LName}" ORDER BY rating DESC LIMIT 0,3`,function(error,result){
+   
+    console.log(result.length)
+    
+    if(error){
+        console.log('Error Occure')
+        res.send(error)
+    }else{
+        res.send(result)
+    }
+})
+}
+
+
+
+exports.searchResultByPrice = (req, res) => {
+    //var category = req.body.ctg;
+    var pName = req.body.name;
+    var name = pName.replace(/\s/g, "").toLowerCase()
+    var LName=pName.toLowerCase()
+
+//select * from testdb where Catagory LIKE "%${category}" AND product_name LIKE "%${pName}" ORDER BY price, rating DESC LIMIT 0,3
+
+con.query(`select * from testdb where LOWER(product_name) LIKE "%${LName}" ORDER BY price LIMIT 0,3`,function(error,result){
+   
+    console.log(result.length)
+    
+    if(error){
+        console.log('Error Occure')
+        res.send(error)
+    }else{
+        res.send(result)
+    }
+})
+}
+
+// exports.filterResultByPriceAndRating = (req, res) => {
+//     //var category = req.body.ctg;
+//     var pName = req.body.name;
+//     var name = pName.replace(/\s/g, "").toLowerCase()
+//     var LName=pName.toLowerCase()
+//     var price = req.body.price
+//     var rating = req.body.rating
+
+//     console.log(price)
+//     console.log(rating)
+// //select * from testdb where Catagory LIKE "%${category}" AND product_name LIKE "%${pName}" ORDER BY price, rating DESC LIMIT 0,3
+
+// con.query(`select * from testdb where LOWER(product_name) LIKE "%${LName}" AND (price <= "%${price}" AND rating <= "%${rating}") LIMIT 0,3`,function(error,result){
+   
+//     console.log(result.length)
+    
+//     if(error){
+//         console.log('Error Occure')
+//         res.send(error)
+//     }else{
+//         res.send(result)
+//     }
+// })
+// }
--- routes/search.js
+++ routes/search.js
@@ -7,4 +7,11 @@ const searchController = require("../controllers/searchController");
 
 router.post("/search", searchController.searchResult);
 
+router.post("/search/rating", searchController.searchResultByRating);
+
+router.post("/search/price", searchController.searchResultByPrice);
+
+//router.post("/search/filter", searchController.filterResultByPriceAndRating);
+
+
 module.exports = router;
